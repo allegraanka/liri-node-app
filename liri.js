ee93b60fe35aef7bcfbd742bcc85949f7fb62ba3
Allegra Anka <allegra.anka@gmail.com>
2019-04-11 18:21:27 -0400

    initial commit, OMDB call done

---
+++ liri.js
@@ -0,0 +1,36 @@
+require("dotenv").config();
+var keys = require("./keys.js");
+var Spotify = require("node-spotify-api");
+var spotify = new Spotify(keys.spotify);
+var axios = require("axios");
+
+var command = process.argv[2];
+
+if (command === "movie-this") {
+    getMovie();
+}
+
+// OMDB functionality
+
+function getMovie() {
+    var baseURL = "http://www.omdbapi.com/";
+    var apiKey = "trilogy";
+    var movieTitle = process.argv[3];
+    axios.get(
+        `${baseURL}?apikey=${apiKey}&t=${movieTitle}`
+    )
+    .then(function (data) {
+        console.log(data);
+        console.log(`Title: ${data.data.Title}`);
+        console.log(`Released: ${data.data.Year}`);
+        console.log(`IMDB Rating: ${data.data.imdbRating}`);
+        console.log(`RT Rating: There is no RT rating...`);
+        console.log(`Country: ${data.data.Country}`);
+        console.log(`Language: ${data.data.Language}`);
+        console.log(`Plot: ${data.data.Plot}`);
+        console.log(`Actors: ${data.data.Actors}`);
+    })
+    .catch(function (error) {
+        console.log(error);
+    });
+}
